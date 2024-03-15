788508059387ba64abee1cb893f5f3edbeac5245
Cornelius Donley <admin@tuesdaycrowd.com>
2024-03-15 13:30:14 -0500

    refactor: Update pretrained model to "BAAI/bge-small-en-v1.5" with quantized false option in SentenceTransformer.from_pretrained function.
    fix: Adjust return value of main function to return outputs from sentenceTransformer.encode method.
    
    feat: Update package name to "@cddigi/sentence-transformers" and version to "0.0.3" in package.json file.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import { env } from "@xenova/transformers";
-
 import { SentenceTransformer } from "./model.js";
 
 async function main() {
-  console.log(env);
   const sentenceTransformer = await SentenceTransformer.from_pretrained(
-    "Supabase/bge-small-en"
+    "BAAI/bge-small-en-v1.5",
+    {
+      quantized: false,
+    },
   );
-  await sentenceTransformer.encode(["Hello world", "How are you guys doing?"]);
+  const outputs = await sentenceTransformer.encode([
+    "Hello world",
+    "How are you guys doing?",
+    "Today is Friday!",
+  ]);
+
+  // @ts-ignore
+  // console.log(Object.keys(outputs));
+  // [ 'last_hidden_state' ]
+
+  return outputs;
 }
 
 main();
